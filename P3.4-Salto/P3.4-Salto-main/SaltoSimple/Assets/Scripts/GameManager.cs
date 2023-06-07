462f32b20aa1111ff2c9e9610914d88ba25a9d0b
9RACHA <66274956+9RACHA@users.noreply.github.com>
2023-06-07 16:38:57 +0200

    Comentarios añadidos

--- P3.4-Salto/P3.4-Salto-main/SaltoSimple/Assets/Scripts/GameManager.cs
+++ P3.4-Salto/P3.4-Salto-main/SaltoSimple/Assets/Scripts/GameManager.cs
@@ -1,39 +1,40 @@
 using Unity.Netcode;
 using UnityEngine;
 
-    public class GameManager : NetworkBehaviour
+public class GameManager : NetworkBehaviour
+{
+    void OnGUI()
     {
-        void OnGUI()
-        {
-            GUILayout.BeginArea(new Rect(10, 10, 300, 300));
-            
-            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
-            {
-                StartButtons();
-            }
-            else
-            {
-                StatusLabels();
-            }
-
-            GUILayout.EndArea();
+        // Establece el área para mostrar los elementos en la interfaz gráfica de usuario (GUI)
+        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+        
+        // Comprueba si no es cliente ni servidor para mostrar los botones de inicio
+        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer){
+            StartButtons();
+        } else {
+            StatusLabels();
         }
-
-        static void StartButtons()
-        {
-            if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
-            if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
-        }
-
-        static void StatusLabels()
-        {
-            
-            var mode = NetworkManager.Singleton.IsHost ?
-                "Host" : NetworkManager.Singleton.IsClient ? "Client" : "Client";
-
-            GUILayout.Label("Transport: " +
-                NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
-            GUILayout.Label("Mode: " + mode);
-        }    
+        // Finaliza el área de la GUI
+        GUILayout.EndArea();
     }
 
+    // Método para mostrar los botones de inicio
+    static void StartButtons(){
+        // Comprueba si se hace clic en el botón "Host" y llama a la función StartHost() del NetworkManager
+        if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
+        // Comprueba si se hace clic en el botón "Client" y llama a la función StartClient() del NetworkManager
+        if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+    }
+
+    // Método para mostrar las etiquetas de estado
+    static void StatusLabels(){
+        // Determina el modo actual (Host o Cliente) y lo muestra en una etiqueta
+        var mode = NetworkManager.Singleton.IsHost ?
+            "Host" : NetworkManager.Singleton.IsClient ? "Client" : "Client";
+        // Muestra el tipo de transporte de red utilizado en una etiqueta
+        GUILayout.Label("Transport: " +
+            NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
+        // Muestra el modo actual (Host o Cliente) en una etiqueta
+        GUILayout.Label("Mode: " + mode);
+    }    
+}
